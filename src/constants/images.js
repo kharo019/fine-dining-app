1a3a9604f8b7c78db6026f88289b4d4e8ecd6ab6
kharo019 <kevinharo019@gmail.com>
2023-10-04 16:05:16 -0600

    Updated Subheading, images.js, aboutus, header, and special menu of both javascript and css

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,82 +1,47 @@
-import images from './images';
+import bg from '../assets/bg.png';
+import chef from '../assets/chef.png';
+import G from '../assets/G.png';
+import gallery01 from '../assets/gallery01.png';
+import gallery02 from '../assets/gallery02.png';
+import gallery03 from '../assets/gallery03.png';
+import gallery04 from '../assets/gallery04.png';
+import knife from '../assets/knife.png';
+import logo from '../assets/logo.png';
+import menu from '../assets/menu.png';
+import overlaybg from '../assets/overlaybg.png';
+import spoon from '../assets/spoon.svg';
+import welcome from '../assets/welcome.png';
+import findus from '../assets/findus.png';
+import laurels from '../assets/laurels.png';
+import award01 from '../assets/award01.png';
+import award02 from '../assets/award02.png';
+import award03 from '../assets/award03.png';
+import award05 from '../assets/award05.png';
+import sign from '../assets/sign.png';
+import quote from '../assets/quote.png';
+import gericht from '../assets/gericht.png';
 
-const wines = [
-  {
-    title: 'Chapel Hill Shiraz',
-    price: '$56',
-    tags: 'AU | Bottle',
-  },
-  {
-    title: 'Catena Malbee',
-    price: '$59',
-    tags: 'AU | Bottle',
-  },
-  {
-    title: 'La Vieillw Rose',
-    price: '$44',
-    tags: 'FR | 750 ml',
-  },
-  {
-    title: 'Rhino Pale Ale',
-    price: '$31',
-    tags: 'CA | 750 ml',
-  },
-  {
-    title: 'Irish Guinness',
-    price: '$26',
-    tags: 'IE | 750 ml',
-  },
-];
-
-const cocktails = [
-  {
-    title: 'Aperol Sprtiz',
-    price: '$20',
-    tags: 'Aperol | Villa Marchesi prosecco | soda | 30 ml',
-  },
-  {
-    title: "Dark 'N' Stormy",
-    price: '$16',
-    tags: 'Dark rum | Ginger beer | Slice of lime',
-  },
-  {
-    title: 'Daiquiri',
-    price: '$10',
-    tags: 'Rum | Citrus juice | Sugar',
-  },
-  {
-    title: 'Old Fashioned',
-    price: '$31',
-    tags: 'Bourbon | Brown sugar | Angostura Bitters',
-  },
-  {
-    title: 'Negroni',
-    price: '$26',
-    tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
-  },
-];
-
-const awards = [
-  {
-    imgUrl: images.award02,
-    title: 'Bib Gourmond',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award01,
-    title: 'Rising Star',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award05,
-    title: 'AA Hospitality',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-  {
-    imgUrl: images.award03,
-    title: 'Outstanding Chef',
-    subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
-  },
-];
-
-export default { wines, cocktails, awards };
+export default {
+  bg,
+  chef,
+  G,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
+  knife,
+  logo,
+  menu,
+  overlaybg,
+  spoon,
+  welcome,
+  findus,
+  laurels,
+  award01,
+  award02,
+  award03,
+  award05,
+  sign,
+  quote,
+  gericht,
+};
