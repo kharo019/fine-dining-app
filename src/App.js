46710f95355e6736269d6209ec090810c72c6b8a
kharo019 <kevinharo019@gmail.com>
2023-10-05 03:12:53 -0600

    Changed images.js and added a png file that is the logo of the project

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, Special
 import { Navbar } from './components';
 import './App.css';
 
+
+
 const App = () => (
   <div>
     <Navbar />
